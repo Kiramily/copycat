4fd01b46707e03d2bba545bd16cd10c4a42b22c6
Kiramily <kiramily@proton.me>
2024-03-14 01:39:20 +0100

    feat: add better error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error,
     fs::{self, create_dir_all, read_dir},
-    io,
+    io::{self, ErrorKind},
     path::Path,
 };
 
@@ -9,8 +9,6 @@ use rayon::ThreadPoolBuilder;
 
 pub fn copy<P: AsRef<Path>>(from: P, to: P, threads: Option<usize>) -> Result<(), Box<dyn Error>> {
     let threads = threads.unwrap_or(num_cpus::get());
-
-    assert!(threads <= num_cpus::get(), "?????");
 
     let pool = ThreadPoolBuilder::new().num_threads(threads).build()?;
 
@@ -27,7 +25,7 @@ fn walk(start: &Path, destination: &Path) -> io::Result<()> {
     }
 
     if !destination.exists() {
-        create_dir_all(&destination).unwrap();
+        create_dir_all(destination).expect("Could not create destination directory");
     }
 
     rayon::scope(|scope| {
@@ -35,11 +33,11 @@ fn walk(start: &Path, destination: &Path) -> io::Result<()> {
             let entry = entry.unwrap();
             let path = entry.path();
 
+            let destination_path = destination.join(path.file_name().unwrap());
+
             if path.is_file() {
-                let destination_path = destination.join(path.file_name().unwrap());
                 scope.spawn(move |_| copy_file(&path, &destination_path));
             } else if path.is_dir() {
-                let destination_path = destination.join(path.file_name().unwrap());
                 scope.spawn(move |_| walk(&path, &destination_path).unwrap());
             }
         }
@@ -52,15 +50,45 @@ fn copy_file(file: &Path, destination: &Path) {
     if destination.exists() {
         // Check Modification Date
 
-        let dest_meta = fs::metadata(&destination).unwrap();
-        let file_meta = fs::metadata(&file).unwrap();
+        let dest_meta = fs::metadata(destination).unwrap();
+        let file_meta = fs::metadata(file).unwrap();
 
         if dest_meta.modified().unwrap() == file_meta.modified().unwrap() {
             return;
+        } else {
+            if dest_meta.len() == file_meta.len() {
+                // Do nothing when the files are the same size
+                return;
+            }
+
+            println!(
+                "removing {} because of unmatched modified dates and different file sizes",
+                destination.display()
+            );
+            fs::remove_file(destination).unwrap_or_else(|_| {
+                panic!(
+                    "Failed to remove file: {} with unmatched modified dates",
+                    destination.display()
+                )
+            });
         }
     }
 
     println!("Copying file: {}", file.display());
 
-    fs::copy(&file, &destination).unwrap();
+    match fs::copy(file, destination) {
+        Ok(_) => {}
+        Err(e) => match e.kind() {
+            ErrorKind::PermissionDenied => eprintln!(
+                "Permission Denied to copy file \"{}\" to \"{}\"",
+                file.display(),
+                destination.display()
+            ),
+            ErrorKind::AlreadyExists => eprintln!(
+                "The destination \"{}\" already exists",
+                destination.display()
+            ),
+            _ => eprintln!("Error while copying: {e}"),
+        },
+    }
 }
